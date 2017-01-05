45f68bc1782b7b76294f29024ab4ef36bb03b209
nishant <nishant.singh87@gmail.com>
2017-01-05 18:51:34 +0530

    app.js

--- learn/ruby/app/app.js
+++ learn/ruby/app/app.js
@@ -1,1 +1,12 @@
-window.app = angular.module("app", []);
+var app = angular.module("app", ["fire-auth"]);
+
+app.value("compileServers", [{name: "default", url: "http://localhost:4567"}]);
+
+app.config(function ($httpProvider) {
+  $httpProvider.defaults.useXDomain       = true;
+  $httpProvider.defaults.headers.common   = {};
+  $httpProvider.defaults.headers.post     = {};
+  $httpProvider.defaults.headers.put      = {};
+  $httpProvider.defaults.headers.patch    = {};
+  $httpProvider.defaults.headers.options  = {};
+});
