90478cca54ec163d14e2f2889f15a4b0350dc8ec
nishant <nishant.singh87@gmail.com>
2017-01-18 01:27:23 +0530

    remaining missions.

--- lab/galaxy/app/missions/missions-service.js
+++ lab/galaxy/app/missions/missions-service.js
@@ -1,20 +1,25 @@
-app.service("missionsService", [function(){
-  var missions = {
-    add: function(planet, vehicle){
-      planet.assign();
-      vehicle.assign();
-      missions.list.push({
-        planet : planet,
-        vehicle: vehicle
-      })
-    },
-    remove: function(index){
-      var mission = missions.list[index];
-      mission.planet.reset();
-      mission.vehicle.unassign();
-      missions.list.splice(index, 1);
-    },
-    list: []
-  };
+app.service("missionsService", [function () {
+  var
+      MAX_MISSIONS = 4,
+      missions = {
+        add: function (planet, vehicle) {
+          planet.assign();
+          vehicle.assign();
+          missions.list.push({
+            planet: planet,
+            vehicle: vehicle
+          })
+        },
+        remaining: function(){
+          return (MAX_MISSIONS - missions.list.length);
+        },
+        remove: function (index) {
+          var mission = missions.list[index];
+          mission.planet.reset();
+          mission.vehicle.unassign();
+          missions.list.splice(index, 1);
+        },
+        list: []
+      };
   return missions;
 }]);
