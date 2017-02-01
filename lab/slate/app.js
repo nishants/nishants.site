404230c882e742e59d56a1079ace25f5cb60dc74
nishant <nishant.singh87@gmail.com>
2017-02-01 17:17:08 +0530

    frame styling.

--- lab/slate/app.js
+++ lab/slate/app.js
@@ -3,6 +3,8 @@
 
   var app = angular.module("slate", []);
 
+	app.constant("IFRAMETIMEOUT", 250);
+
 	app.run(["$http", "DeckService", "$rootScope", function($http, DeckService, $rootScope){
 		$http.get("data/cards.json").then(DeckService.load)
 		$rootScope.deck = DeckService;
--- lab/slate/modal/modal-service.js
+++ lab/slate/modal/modal-service.js
@@ -1,17 +1,23 @@
-app.service("modalService",["DeckService", "$timeout",function(DeckService, $timeout){
+app.service("modalService",["DeckService", "$timeout", "IFRAMETIMEOUT",function(DeckService, $timeout, IFRAMETIMEOUT){
 	var modal = {
 		_show 	: false,
+		_timer  : null,
 		_frame   : {
 			_ready 	: false,
 			_src  	: null
 		},
 		show: function(src){
 			modal._show = true;
-			modal._frame._src = src;
+			modal._timer = $timeout( function(){
+				modal._frame._src = src;
+			}, IFRAMETIMEOUT);
 		},
 		close: function(){
 			modal._frame._ready = false;
 			modal._frame._src  	= null;
+			if(modal._timer){
+				$timeout.cancel(modal._timer);
+			}
 			DeckService.unselect();
 		},
 		ready: function(){
