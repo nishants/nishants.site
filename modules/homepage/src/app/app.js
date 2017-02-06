36b11675fa9409731a3c31889ece4c08533452cc
nishant <nishant.singh87@gmail.com>
2017-02-06 08:14:29 +0530

    adding customized scroll bar.

--- modules/homepage/src/app/app.js
+++ modules/homepage/src/app/app.js
@@ -2,5 +2,5 @@ var app = angular.module("nishants", []);
 app.run(["$timeout", "$rootScope", function($timeout, $rootScope){
 	$timeout(function(){
 		$rootScope.splash  = {close: true};
-	}, 2000);
+	}, 300);
 }]);
