4549f3d1dbd8c758d98fad1a512535eb39402df1
nishant <nishant.singh87@gmail.com>
2017-02-06 09:09:57 +0530

    set scroll progress.

--- modules/homepage/src/app/app.js
+++ modules/homepage/src/app/app.js
@@ -4,3 +4,25 @@ app.run(["$timeout", "$rootScope", function($timeout, $rootScope){
 		$rootScope.splash  = {close: true};
 	}, 300);
 }]);
+
+$(document).ready(function(){
+	var	setScroll = function(){
+				var
+						$scrollable 		 = $(".scroll-container").first(),
+						viewPortHeight   = $scrollable.height(),
+						height 					 = $scrollable[0].scrollHeight,
+						scrollTop        = $scrollable.scrollTop(),
+						maxScrollTop     = height - viewPortHeight,
+						scrollProgress   = scrollTop/maxScrollTop,
+						$pointer         = $(".scroll-bar > .progress").first(),
+						pointerHeight    = $pointer.height(),
+						offset 				   = scrollProgress * (viewPortHeight - pointerHeight);
+
+				$(".scroll-bar > .progress").css("transform", "translateY("+offset+"px)");
+				console.log("Progerss :  " + scrollProgress)
+				console.log("offset :  " + offset)
+			};
+
+	$(".scroll-container").on("scroll", setScroll);
+	$(window).on("resize", setScroll);
+});
