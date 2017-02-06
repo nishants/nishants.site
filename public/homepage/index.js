0d0f1ed4f589d4631255b36418d989f39357d804
nishant <nishant.singh87@gmail.com>
2017-02-06 09:21:44 +0530

    add public dir

--- public/homepage/index.js
+++ public/homepage/index.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var app = angular.module("nishants", []);
 app.run(["$timeout", "$rootScope", function($timeout, $rootScope){
 	$timeout(function(){
@@ -19,8 +20,13 @@ $(document).ready(function(){
 						offset 				   = scrollProgress * (viewPortHeight - pointerHeight);
 
 				$(".scroll-bar > .progress").css("transform", "translateY("+offset+"px)");
+				console.log("Progerss :  " + scrollProgress)
+				console.log("offset :  " + offset)
 			};
 
 	$(".scroll-container").on("scroll", setScroll);
 	$(window).on("resize", setScroll);
 });
+},{}],2:[function(require,module,exports){
+require("./app/app.js");
+},{"./app/app.js":1}]},{},[2]);
