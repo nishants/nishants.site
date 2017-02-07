eb9a99eac954d69c72cdf9b1fc7937b532d2ffc4
nishant <nishant.singh87@gmail.com>
2017-02-07 14:24:47 +0530

    typography.

--- modules/homepage/src/app/app.js
+++ modules/homepage/src/app/app.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
 		setState("top-bar", 		isGone($(".intro  .profile-image")[0]));
 		setState("name", 				isUnderTitleBar($(".intro  .name")[0]));
 
-		setState("navigation"	,   ifIssUnderTitleBar(50,$(".intro  .navigation > .design")[0]));
-		setState("design"	    , 	ifIssUnderTitleBar(50,$(".intro  .navigation > .design")[0]));
-		setState("connect"	  , 	ifIssUnderTitleBar(50,$(".intro  .navigation > .connect")[0]));
-		setState("experience" , 	ifIssUnderTitleBar(50,$(".intro  .navigation > .experience")[0]));
-		setState("development", 	ifIssUnderTitleBar(50,$(".intro  .navigation > .development")[0]));
-		setState("coaching"   , 	ifIssUnderTitleBar(50,$(".intro  .navigation > .coaching")[0]));
-		setState("slate"      , 	ifIssUnderTitleBar(50,$("#slate")[0]));
+		setState("navigation"	,   ifIssUnderTitleBar(20,$(".intro  .navigation > .design")[0]));
+		setState("design"	    , 	ifIssUnderTitleBar(20,$(".intro  .navigation > .design")[0]));
+		setState("connect"	  , 	ifIssUnderTitleBar(20,$(".intro  .navigation > .connect")[0]));
+		setState("experience" , 	ifIssUnderTitleBar(20,$(".intro  .navigation > .experience")[0]));
+		setState("development", 	ifIssUnderTitleBar(20,$(".intro  .navigation > .development")[0]));
+		setState("coaching"   , 	ifIssUnderTitleBar(20,$(".intro  .navigation > .coaching")[0]));
+		setState("slate"      , 	ifIssUnderTitleBar(20,$("#slate")[0]));
 	});
 
 	setTimeout(function(){
