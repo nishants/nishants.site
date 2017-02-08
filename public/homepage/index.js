8da10e35041182a64e44ede99f8964077fa6df39
nishant <nishant.singh87@gmail.com>
2017-02-08 19:41:25 +0530

    unstick timeline if passed over.

--- public/homepage/index.js
+++ public/homepage/index.js
@@ -18,9 +18,36 @@ $(document).ready(function(){
 
 			ifIssUnderTitleBar = function (offset, element) {
 				return element.getBoundingClientRect().top < ($(".top-bar > .background").height() - offset);
-			}
+			},
 			isUnderTitleBar = function (element) {
 				return element.getBoundingClientRect().top < ($(".top-bar > .background").height() - offset);
+			},
+			viewingExperience = function () {
+				var underTitleBar = ifIssUnderTitleBar(0, $(".timeline .timeline-header")[1]),
+						hasPassedOver = ifIssUnderTitleBar(-100, $("#timeline-ends")[0]);
+				return underTitleBar && !hasPassedOver;
+			},
+			currentExperienceIndex = function() {
+				var topBarHeight    = 108,
+						sectionOffset   = 50,
+						selectionOffset = topBarHeight + sectionOffset,
+						unSelectionOffset = 122,
+						list = $(".timeline-body").last().find(".period-list > li").toArray().reverse(),
+						index ;
+
+				for(var i =0; i < list.length; i++ ){
+					var e = list[i],
+							isUnderTitleBar = e.getBoundingClientRect().top < selectionOffset,
+							isPassingBy = $(e).find(".card:last-child");
+					if(isUnderTitleBar){
+						index = i;
+						break;
+					}
+				}
+				return list.length - index -1;
+			},
+			stickExperienceAt = function(index){
+				console.log("Reading Exp at" + index)
 			};
 
 	$(window).on("scroll", function(){
@@ -36,11 +63,14 @@ $(document).ready(function(){
 		setState("slate"      , 	ifIssUnderTitleBar(20,$("#slate")[0]));
 		setState("social"     , 	ifIssUnderTitleBar(-10,$(".intro  ul.contact.social")[0]));
 
-		var underTitleBar = ifIssUnderTitleBar(0, $(".timeline .timeline-header")[1]),
-				hasPassedOver = ifIssUnderTitleBar(-100, $("#timeline-ends")[0]),
-				showExperience = underTitleBar && !hasPassedOver;
 
-		setState("stick-experience"  , 	showExperience);
+
+		var show = viewingExperience();
+		setState("stick-experience"  , 	show);
+		if(show){
+			stickExperienceAt(currentExperienceIndex());
+
+		}
 	});
 
 	setTimeout(function(){
