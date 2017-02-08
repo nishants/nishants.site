9904f0eef0a9d9dc3d623d2b6a70a674c284a3e8
nishant <nishant.singh87@gmail.com>
2017-02-08 11:52:42 +0530

    adding compact card.

--- public/homepage/index.js
+++ public/homepage/index.js
@@ -383,26 +383,27 @@ angular.module("timeline").service("ProfileService", [function () {
 					title: 'Technical Consultant',
 					description: 'Worked with some great cross functional teams.',
 					organisation: 'ThoughtWorks',
+					logo : "public/images/logos/tw.png",
 					tags : ["consulting", "mentoring", "evolutionary design", "test driven development", "tdd"],
 					period  : {from: {year: 2015, month: 6}, to: {year: 2017, month: 2}},
 					projects : [
 						{
 							title: 'Caterpillar',
-							description: 'Position',
+							description: 'Single-platform to manage parts sales commercials, across their global distribution network of dealers.',
 							tags : [],
 							tech : ["java", "angular", "scss", "micro services"],
-							period  : {from: {year: 2015, month: 6}, to: {year: 2017, month: 2}},
+							period  : {from: {year: 2012, month: "Sep"}, to: {year: "2014", month: "Oct"}},
 						},
 						{
 							title: 'Health Care At Home',
-							description: 'Position',
-							period  : {from: {year: 2015, month: 6}, to: {year: 2017, month: 2}},
+							description: 'Mobile webapp for a Delhi based health startup. App helped nurses to collect data and generate reports on patients.',
+							period  : {from: {year: 2012, month: "April"}, to: {year: 2012, month: "Sep"}},
 							tags : []
 						},
 						{
 							title: 'UNICEF Uganda',
 							description: 'Position',
-							period  : {from: {year: 2015, month: 6}, to: {year: 2017, month: 2}},
+							period  : {from: {year: 2011, month: "Dec"}, to: {year: 2012, month: "April"}},
 							tags : []
 						}
 
