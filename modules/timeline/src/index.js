6377d8f79773c69bbd8bc93e6395c27e2cbc2735
nishant <nishant.singh87@gmail.com>
2017-02-13 12:57:36 +0530

    remove timeline module.

--- modules/timeline/src/index.js
+++
@@ -1,5 +0,0 @@
-require("./app/app.js");
-require("./app/timeline/timeline-controller");
-require("./app/timeline/search/search");
-require("./app/timeline/search/search-controller");
-require("./app/timeline/profile-service");
