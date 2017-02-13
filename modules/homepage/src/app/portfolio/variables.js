3be3de74d338e3a0f7a2fcbfb9c4a25b4d8ed91b
nishant <nishant.singh87@gmail.com>
2017-02-13 16:01:59 +0530

    adding cardbon fiber theme.

--- modules/homepage/src/app/portfolio/variables.js
+++ modules/homepage/src/app/portfolio/variables.js
@@ -3,7 +3,7 @@ app.constant("IFRAMETIMEOUT", 250);
 app.constant("GRID_CONFIG", {
 	topOffset: 0,
 	colWidth : 325,
-	gridBoxMarginX: -1, // To overlap borders
-	gridBoxMarginY: -1, // To overlap borders
+	gridBoxMarginX: 10, // To overlap borders
+	gridBoxMarginY: 10, // To overlap borders
 	domUpdateDelay: 500
 });
