460cb0f6359368bdbe9d268654ec8aaf95a2c263
nishant <nishant.singh87@gmail.com>
2017-02-15 18:50:31 +0530

    update project structure.

--- modules/homepage/src/app/portfolio/variables.js
+++
@@ -1,9 +0,0 @@
-app.constant("IFRAMETIMEOUT", 250);
-
-app.constant("GRID_CONFIG", {
-	topOffset: 0,
-	colWidth : 325,
-	gridBoxMarginX: 10, // To overlap borders
-	gridBoxMarginY: 10, // To overlap borders
-	domUpdateDelay: 500
-});
--- modules/homepage/src/app/variables.js
+++
@@ -1,5 +0,0 @@
-angular.module("nishants").constant("CONTACT_CONFIG", {
-	email  : "nishant.singh87@gmail.com",
-	subject: "Hi !",
-	body   : ""
-});
