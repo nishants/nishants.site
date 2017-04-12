d5c2775477d4b7ab255e1256024fbb34b1f8e3a6
nishant <nishant.singh87@gmail.com>
2017-04-12 19:30:57 +0530

    update zinc oxide

--- public/zinc/re-imagine/index.js
+++ public/zinc/re-imagine/index.js
@@ -1,1 +1,163 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){window.app=angular.module("galaxy",[])},{}],2:[function(require,module,exports){app.directive("dropDown",[function(){return{restrict:"C",scope:true,template:"<div class='dropdown-inner' ng-mouseleave='dropdown.show(false)' ng-mouseenter='dropdown.show(true)'><div class='current' ><label class='value' ng-bind='dropdown.current.value' ng-show='dropdown.current.value'></label> <label class='placeholder' ng-bind='dropdown.name' ng-hide='dropdown.current.value'></label><div class='caret fa fa-chevron-down'></div></div><ul class='options'><li ng-repeat='option in dropdown.options' ng-bind='option.value' ng-click='dropdown.select(option)'></li><li ng-click='dropdown.clear()' ng-show='dropdown.reset' ng-bind='dropdown.reset'></li></ul></div>",link:function(scope,element,attrs){var dropdown={name:attrs.name,active:false,reset:scope.$eval(attrs.reset)||attrs.reset,current:{value:scope.$eval(attrs.value)},options:scope.$eval(attrs.options),select:function(option){dropdown.current.value=option.value;dropdown.show(false)},show:function(value){dropdown.active=value;value?element.addClass("active"):element.removeClass("active")},clear:function(){dropdown.current={value:null};dropdown.show(false)}};scope.dropdown=dropdown}}}])},{}],3:[function(require,module,exports){app.value("remote","https://findfalcone.herokuapp.com");app.value("requestConfig",{headers:{Accept:"application/json","Content-Type":"application/json"}})},{}],4:[function(require,module,exports){app.value("planetIcons",{Donlon:"images/planets/Donlon.png",Enchai:"images/planets/Enchai.ico",Jebing:"images/planets/Jebing.png",Sapir:"images/planets/Sapir.png",Lerbin:"images/planets/Lerbin.jpg",Pingasor:"images/planets/Pingasor.ico",other:"images/planets/other-planet.png"});app.value("vehicleIcons",{"Space pod":"images/vehicles/space-pod.jpg","Space rocket":"images/vehicles/space-rocket.png","Space shuttle":"images/vehicles/shutle.jpeg","Space ship":"images/vehicles/spaceship.png",other:"images/vehicles/other-vehicle.jpg"})},{}],5:[function(require,module,exports){require("./app/app.js");require("./app/variables.js");require("./app/config.js");require("./app/components/drop-down")},{"./app/app.js":1,"./app/components/drop-down":2,"./app/config.js":3,"./app/variables.js":4}]},{},[5]);
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+window.app = angular.module("zinc", ['ui.router']);
+},{}],2:[function(require,module,exports){
+angular.module("zinc").directive("dropDown", [function () {
+
+	return {
+		restrict: "C",
+		scope   : true,
+		template: "<div class='dropdown-inner' ><div class='current' ng-click='dropdown.show(!dropdown.active)'><label class='value' ng-bind='dropdown.current.value' ng-show='dropdown.current.value'></label> <label class='placeholder' ng-bind='dropdown.name' ng-hide='dropdown.current.value'></label><div class='caret fa fa-chevron-down'></div></div><ul class='options'><li ng-repeat='option in dropdown.options' ng-bind='option.value' ng-click='dropdown.select(option)'></li><li ng-click='dropdown.clear()' ng-show='dropdown.reset' ng-bind='dropdown.reset'></li></ul></div>",
+		link    : function (scope, element, attrs) {
+			var dropdown = {
+				name: attrs.name,
+				active: false,
+				reset: scope.$eval(attrs.reset) || attrs.reset,
+				current: {
+					value: scope.$eval(attrs.value)
+				},
+				options: scope.$eval(attrs.options),
+				select: function (option) {
+					dropdown.current.value = option.value;
+					dropdown.show(false);
+				},
+				show: function(value){
+					dropdown.active = value;
+					value ? element.addClass("active") : element.removeClass("active");
+				},
+				clear: function(){
+					dropdown.current = {value: null};
+					dropdown.show(false);
+				}
+			};
+			scope.dropdown = dropdown;
+
+		}
+	};
+}]);
+},{}],3:[function(require,module,exports){
+angular.module("zinc").run(["$rootScope", "stateMessages" ,function($rootScope, stateMessages){
+	var state = {
+			name				: "",
+			loading     : null,
+			message     : "Welcome Onboard !",
+			error     	: null,
+			loadingNext	: function(name, message){
+				state.message = message;;
+				state.loading = name;
+				state.error   = null;
+			},
+			done   			: function(stateName){
+				state.loading = null;
+				state.message = null;
+				state.name    = stateName;
+			},
+			failed: function(error){
+				state.loading = null;
+				state.message = null;
+				state.error   = error;
+			}
+		},
+		nameFor = function(state){
+			return state.replace(/\./g, "-");
+		};
+
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options){
+		state.loadingNext(nameFor(toState.name), stateMessages[toState.name]);
+	});
+
+	$rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams){
+		state.failed("Unknown Error");
+	});
+
+	$rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){
+		state.failed("Unknown Error");
+	});
+
+	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+		state.done(nameFor(toState.name));
+	});
+
+	$rootScope.state = state;
+}]);
+},{}],4:[function(require,module,exports){
+app.value("remote", "https://findfalcone.herokuapp.com");
+app.value("requestConfig", {headers: {Accept: "application/json", "Content-Type": "application/json"}});
+
+},{}],5:[function(require,module,exports){
+angular.module("zinc").config(["$stateProvider", "$urlRouterProvider", "$locationProvider",function($stateProvider, $urlRouterProvider, $locationProvider){
+
+	$urlRouterProvider.otherwise('/vocab');
+	$stateProvider
+			.state('home', {
+				url: '/home',
+				templateUrl: 'assets/templates/home-template.html'
+			})
+			.state('vocab', {
+				url: '/vocab',
+				templateUrl: 'assets/templates/vocab-list-template.html',
+				resolve : {
+					list: ["VocabService", function(VocabService){
+						return VocabService.fetchMySets();
+					}]
+				},
+				controller: ["$scope", "list", function($scope, list){
+					$scope.games = list;
+				}]
+			})
+			.state('vocab.view', {
+				url: '/:id/view',
+				templateUrl: 'assets/templates/vocab-view-template.html',
+				resolve: {
+					vocabset: [
+						"$stateParams",
+						"VocabService",
+						function($stateParams, VocabService){
+							return VocabService.getById($stateParams.id);
+						}
+					]
+				},
+				controller: ["$scope", "vocabset", function($scope, vocabset){
+					$scope.vocabset = vocabset;
+				}]
+			});
+
+	$locationProvider.html5Mode({
+		//enabled: true,
+		//requireBase: false
+	});
+}]);
+},{}],6:[function(require,module,exports){
+app.value("stateMessages", {
+	"vocab.view"  : "Opening Vocabulary Set",
+	"default"     : "Loading ",
+});
+},{}],7:[function(require,module,exports){
+angular.module("zinc").service("VocabService", ["$http", function ($http) {
+	var service = {
+		list : [],
+		page : {
+			index: -1,
+			size : 10
+		},
+		fetchMySets: function(){
+			return $http.get("assets/data/vocab-sets.json").then(function(response){
+				return response.data.list;
+			})
+		},
+		getById: function(id){
+			return $http.get("assets/data/vocab-set-:id.json".replace(":id", id)).then(function(response){
+				return response.data.vocabset;
+			});
+		}
+	};
+	return service;
+}]);
+},{}],8:[function(require,module,exports){
+require("./app/app");
+require("./app/variables");
+require("./app/config");
+require("./app/routes");
+require("./app/components/forms/drop-down");
+require("./app/components/state-loader/state-loader");
+require("./app/vocabulary/vocab-service");
+},{"./app/app":1,"./app/components/forms/drop-down":2,"./app/components/state-loader/state-loader":3,"./app/config":4,"./app/routes":5,"./app/variables":6,"./app/vocabulary/vocab-service":7}]},{},[8]);
