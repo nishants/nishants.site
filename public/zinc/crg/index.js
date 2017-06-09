423bbb318c75471506bf83fb6f05ccb460925201
nishant <nishant.singh87@gmail.com>
2017-06-09 15:34:10 +0530

    crg standalone

--- public/zinc/crg/index.js
+++ public/zinc/crg/index.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-window.app = angular.module("zinc", ['ui.router', 'ngDraggable']);
+window.app = angular.module("crg", ['ui.router', 'ngDraggable']);
 },{}],2:[function(require,module,exports){
 app.directive("typewriter",["$timeout", function($timeout){
   return {
@@ -52,7 +52,7 @@ app.directive("typewriter",["$timeout", function($timeout){
   };
 }])
 },{}],3:[function(require,module,exports){
-angular.module("zinc").directive("dropDown", [function () {
+app.directive("dropDown", [function () {
 
 	return {
 		restrict: "C",
@@ -86,7 +86,7 @@ angular.module("zinc").directive("dropDown", [function () {
 	};
 }]);
 },{}],4:[function(require,module,exports){
-angular.module("zinc").run(["$rootScope", "stateMessages" ,function($rootScope, stateMessages){
+app.run(["$rootScope", "stateMessages" ,function($rootScope, stateMessages){
 	var state = {
 			name				: "",
 			loading     : null,
@@ -1013,109 +1013,10 @@ app.service("YesNoEditor", ['SceneLoader', function (SceneLoader) {
   return textInputEditor;
 }]);
 },{}],40:[function(require,module,exports){
-angular.module('zinc').controller('GamePlayController', ["$scope", "deckParticipation", "GamePlayService", function($scope, deckParticipation, GamePlayService){
-			$scope.gamePlay = GamePlayService.create(deckParticipation);
-
-			$scope.gameplan = deckParticipation.gameplan;
-			$scope.deck     = deckParticipation.deck;
-			$scope.vocabset    = {id: 1};
-			$scope.timer    = {timeLimit: '15s'};
-			$scope.user     = {
-				points: 100,
-				timer : "00:19",
-			};
-			$scope.gamePlay.next();
-		}]
-);
-},{}],41:[function(require,module,exports){
-angular.module("zinc").service("GamePlayService", ["$http", function ($http) {
-	var toWord = function(data){
-		return {
-			name: data.word,
-			audio: data.pronunciation_audio,
-			synonym: {
-				options: data.synonym_option_names.map(function (name) {
-					return {
-						name: name,
-						correct :name ==  data.correct_synonym,
-					};
-				}),
-			},
-			image: {
-				options: data.image_options.map(function (option) {
-					return {
-						name: option.word,
-						url : option.image_medium_url,
-						correct: data.image_medium_url == option.image_medium_url
-					};
-				}),
-			}
-		};
-	};
-	return {
-		create: function(data){
-			var toggleMode = 0;
-			gamePlay = {
-				vocabset: {
-					id: 2,
-					title: data.deck.set_title,
-					image: data.deck.set_image_url
-				},
-				words: data.deck.words.map(toWord),
-				challenges: data.deck.words.map(function (word) {
-					var word = toWord(word);
-					toggleMode++;
-					return {
-						mode: {
-							image  : (toggleMode%2 == 1),
-							synonym: (toggleMode%2 == 0)
-						},
-						word: word,
-						skipped: false,
-						current: false,
-					}
-				}),
-				current: null,
-				_current_index: -1,
-				next: function(){
-					gamePlay.current = gamePlay.challenges[++gamePlay._current_index];
-				},
-				hasNext: function(){
-					return gamePlay._current_index < gamePlay.challenges.length - 1;
-				},
-				skip: function () {
-					gamePlay.current.skipped = true;
-					gamePlay.next();
-				}
-			};
-			return gamePlay;
-		}
-	}
-}]);
-},{}],42:[function(require,module,exports){
-angular.module('zinc').controller('DropSquaresController', function ($scope) {
-	$scope.onDrop = function (option) {
-		!option.correct ? alert("Wrong answer"): "";
-		$scope.gamePlay.next();
-	};
-});
-},{}],43:[function(require,module,exports){
-angular.module("zinc").config(["$stateProvider", "$urlRouterProvider", "$locationProvider",function($stateProvider, $urlRouterProvider, $locationProvider){
+app.config(["$stateProvider", "$urlRouterProvider", "$locationProvider",function($stateProvider, $urlRouterProvider, $locationProvider){
 
 	$urlRouterProvider.otherwise('/crg');
 	$stateProvider
-			.state('home', {
-				url: '/home',
-				templateUrl: 'assets/templates/home-template.html'
-			})
-			.state('articles', {
-				url: '/articles',
-				templateUrl: 'assets/templates/articles-template.html'
-			})
-			.state('reports', {
-				url: '/reports',
-				templateUrl: 'assets/templates/reports-template.html'
-			})
 			.state('crg', {
 				url: '/crg',
 				templateUrl: 'assets/templates/crg-template.html'
@@ -1169,59 +1070,11 @@ angular.module("zinc").config(["$stateProvider", "$urlRouterProvider", "$locatio
             return gameData;
           }]
         }
-      })
-			.state('vocab', {
-				url: '/vocab',
-				templateUrl: 'assets/templates/vocab-list-template.html',
-				resolve : {
-					list: ["VocabService", function(VocabService){
-						return VocabService.fetchMySets();
-					}]
-				},
-				controller: ["$scope", "list", function($scope, list){
-					$scope.games = list;
-				}]
-			})
-			.state('vocab.view', {
-				url: '/:id/view',
-				templateUrl: 'assets/templates/vocab-view-template.html',
-				resolve: {
-					vocabset: [
-						"$stateParams",
-						"VocabService",
-						function($stateParams, VocabService){
-							return VocabService.getById($stateParams.id);
-						}
-					]
-				},
-				controller: ["$scope", "vocabset", function($scope, vocabset){
-					$scope.vocabset = vocabset;
-				}]
-			})
-			.state('vocab.play', {
-				url: '/:vocabId/deck/:deckId/play',
-				templateUrl: 'assets/templates/game-play-template.html',
-				resolve: {
-					deckParticipation: [
-						"$stateParams",
-						"VocabService",
-						function($stateParams, VocabService){
-							return  VocabService.getGamePlanFor($stateParams.vocabId, $stateParams.deckId );
-						}
-					]
-				},
-				controller: "GamePlayController"
-			});
-
-	$locationProvider.html5Mode({
-		//enabled: true,
-		//requireBase: false
-	});
+      });
 }]);
 
-},{}],44:[function(require,module,exports){
+},{}],41:[function(require,module,exports){
 app.value("stateMessages", {
-  "vocab.view"  : "Opening Vocabulary Set",
   "default"     : "Loading ",
 });
 
@@ -1266,33 +1119,7 @@ app.value("SceneLoader", {
     label: "Exit"
   }
 });
-},{}],45:[function(require,module,exports){
-angular.module("zinc").service("VocabService", ["$http", function ($http) {
-	var service = {
-		list : [],
-		page : {
-			index: -1,
-			size : 10
-		},
-		fetchMySets: function(){
-			return $http.get("assets/data/vocab-sets.json").then(function(response){
-				return response.data.list;
-			})
-		},
-		getById: function(id){
-			return $http.get("assets/data/vocab-set-:id.json".replace(":id", id)).then(function(response){
-				return response.data.vocabset;
-			});
-		},
-		getGamePlanFor: function(vocabId, deckId){
-			return $http.get("assets/data/vocab-set-:vocabId/deck-participation-:deckId.json".replace(":vocabId", vocabId).replace(":deckId", deckId)).then(function(response){
-				return response.data.deck_participation;
-			});
-		}
-	};
-	return service;
-}]);
-},{}],46:[function(require,module,exports){
+},{}],42:[function(require,module,exports){
 require("./app/app");
 require("./app/variables");
 require("./app/config");
@@ -1300,9 +1127,6 @@ require("./app/routes");
 require("./app/components/forms/drop-down");
 require("./app/components/state-loader/state-loader");
 require("./app/components/animations/typewriter");
-require("./app/vocabulary/vocab-service");
-require("./app/game-play/modes/drop-squares/drop-squares-controller");
-require("./app/game-play/game-play-service");
-require("./app/game-play/game-play-controller");
+
 require("./app/crg/crg");
-},{"./app/app":1,"./app/components/animations/typewriter":2,"./app/components/forms/drop-down":3,"./app/components/state-loader/state-loader":4,"./app/config":5,"./app/crg/crg":8,"./app/game-play/game-play-controller":40,"./app/game-play/game-play-service":41,"./app/game-play/modes/drop-squares/drop-squares-controller":42,"./app/routes":43,"./app/variables":44,"./app/vocabulary/vocab-service":45}]},{},[46]);
+},{"./app/app":1,"./app/components/animations/typewriter":2,"./app/components/forms/drop-down":3,"./app/components/state-loader/state-loader":4,"./app/config":5,"./app/crg/crg":8,"./app/routes":40,"./app/variables":41}]},{},[42]);
